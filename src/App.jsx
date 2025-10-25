466ae68f58952cf59ac3cb9bf6629a143d315d58
rishabhIndiazona <rishabh.indiazona@gmail.com>
2025-10-25 16:58:21 +0530

    intital setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="maintenance-container">
+      <h1>🛠️ Site Under Maintenance</h1>
+      <p>
+        We’re currently performing some scheduled maintenance. <br />
+        Please check back soon — we’ll be live again shortly!
+      </p>
+      <small>Thank you for your patience.</small>
+    </div>
+  );
+}
+
+export default App;
